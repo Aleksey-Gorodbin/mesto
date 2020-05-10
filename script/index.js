78ccbc7c01d9bac2f640fc34b096c20ffa2fd7c1
Aleksey Gorodbin <89o34681381@mail.ru>
2020-05-10 13:20:11 +0300

    changed all mistakes

--- script/index.js
+++ script/index.js
@@ -1,29 +1,29 @@
-let popup = document.querySelector(".popup");
-console.log(popup);
-
+const POPUP = document.querySelector(".popup");
 let buttonEdit = document.querySelector(".profile__button-edit");
-buttonEdit.addEventListener("click", function openForm() {
-  popup.classList.add("popup_opened");
-});
-
 let buttonClose = document.querySelector(".popup__button-close");
-buttonClose.addEventListener("click", closeForm);
-
-function closeForm() {
-  popup.classList.remove("popup_opened");
-}
-
 let nameInput = document.querySelector("#name");
 let jobInput = document.querySelector("#position");
 let nameText = document.querySelector(".profile__name");
 let positionText = document.querySelector(".profile__position");
 let formElement = document.querySelector(".popup__container");
+
+function openForm() {
+  POPUP.classList.add("popup_opened");
+  nameInput.value = nameText.textContent;
+  jobInput.value = positionText.textContent;
+}
+
+function closeForm() {
+  POPUP.classList.remove("popup_opened");
+}
+
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let nameInput = document.querySelector("#name");
-  let jobInput = document.querySelector("#position");
   nameText.textContent = nameInput.value;
   positionText.textContent = jobInput.value;
   closeForm();
 }
+
 formElement.addEventListener("submit", formSubmitHandler);
+buttonClose.addEventListener("click", closeForm);
+buttonEdit.addEventListener("click", openForm);
